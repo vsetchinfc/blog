13d67e9b958efecff6d9e70a7a57191ed7a534f0
Vlad Setchin <vlad.setchin@gmail.com>
2020-01-08 08:57:15 +1100

    WIP: Site Layout

---
+++ src/.vuepress/theme/components/Head.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        This is a Header
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
--- src/.vuepress/theme/layouts/Layout.vue
+++ src/.vuepress/theme/layouts/Layout.vue
@@ -1,13 +1,24 @@
 <template>
-    <div>Hello!</div>
+    <div>
+        <Head />
+        Hello!
 
-    <IDCard />
+        <IDCard />
+    </div>
 </template>
 
 <script>
+import IDCard from "@theme/components/IDCard.vue"
+import Head from "@theme/components/Head.vue"
+
 export default {
     components: {
-        
+        IDCard,
+        Head
     }
 }
 </script>
+
+<style lang="stylus" scoped>
+
+</style>
