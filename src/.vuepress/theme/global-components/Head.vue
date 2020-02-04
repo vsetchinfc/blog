32662f21ad1d2c5d1b0a460fd14b8e5c4fac075b
Vlad Setchin <vlad.setchin@gmail.com>
2020-02-05 08:31:32 +1100

    WIP: Header layout

--- src/.vuepress/theme/global-components/Head.vue
+++ src/.vuepress/theme/global-components/Head.vue
@@ -1,22 +1,17 @@
 <template>
-  <section id="header-wrapper">
-    <header id="header">
-      <div class="header-wrapper">
-        <div class="title">
-          <a href="/" class="home-link">{{ $site.title }}</a>
-        </div>
-        <div class="header-right-wrap">
-          Search: <SearchBox />
-        </div>
+  <header id="header">
+    <div class="header-wrapper">
+      <div class="title">
+        <a href="/" class="home-link">{{ $site.title }}</a>
       </div>
-      <div class="header-wrapper">
-        Middle Section
+      <div class="header-right-wrap">
+        Search:
+        <SearchBox />
       </div>
-      <div>
-        Nav Bar
-      </div>    
-    </header>
-  </section>
+    </div>
+    <div class="header-wrapper">Middle Section</div>
+    <div class="header-wrapper">Nav Bar</div>
+  </header>
 </template>
 
 <script>
@@ -32,7 +27,7 @@ export default {
 
 #header {
   z-index: 20;
-  position: fixed;
+  position: relative;
   top: 0;
   width: 100vw;
   box-sizing: border-box;
@@ -41,16 +36,32 @@ export default {
   margin: 0 auto;
   box-shadow: 0 5px 20px rgba(0, 0, 0, 0.02), 0 6px 6px rgba(0, 0, 0, 0.03);
   transition: all 1s cubic-bezier(0.25, 0.8, 0.25, 1);
-  height: $navbarHeight;
-
-  ol, ul {
-    list-style: none;
-    margin: 0;
-    padding: 0;
-  }
+  height: 250px;
 
   &:hover {
     box-shadow: 0 5px 20px rgba(0, 0, 0, 0.04), 0 6px 6px rgba(0, 0, 0, 0.08);
+  }
+}
+
+ol, ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.title {
+  /* flex 0 0 200px */
+  color: #FFFFFF;
+  font-size: 22px;
+  margin: 0;
+  letter-spacing: 2px;
+  display: block;
+  text-transform: uppercase;
+
+  a {
+    color: #FFFFFF;
+    font-weight: bold;
+    text-decoration: none;
   }
 }
 
@@ -59,87 +70,72 @@ export default {
   display: flex;
   line-height: 50px;
   height: 100%;
+  position: relative;
+}
 
-  .title {
-    /* flex 0 0 200px */
-    color: #FFFFFF;
-    font-size: 22px;
-    margin: 0;
-    letter-spacing: 2px;
-    display: block;
-    text-transform: uppercase;
+.header-right-wrap {
+  flex: 1;
+  display: flex;
+  color: #ffffff;
+  justify-content: flex-end;
+}
+
+.nav {
+  flex: 0 0 auto;
+  display: flex;
+  margin: 0;
+  align-items: center;
+
+  .nav-item {
+    margin-left: 20px;
 
     a {
-      color: #FFFFFF;
-      font-weight: bold;
+      font-size: 18px;
+      color: lighten($accentColor, 40%);
       text-decoration: none;
+      transition: color 0.3s;
+
+      &:hover {
+        color: lighten($accentColor, 80%);
+      }
+
+      &.nav-link.router-link-exact-active.router-link-active {
+        color: lighten($accentColor, 80%);
+      }
+    }
+  }
+}
+
+.search-box {
+  margin-left: 20px;
+
+  input {
+    min-width: 100px;
+    border-radius: 20px;
+    transition: all 0.5s;
+    color: $accentColor;
+
+    &:hover {
+      border: 1px solid $accentColor;
+      box-shadow: 0 0 5px $accentColor;
     }
   }
 
-  .header-right-wrap {
-    flex: 1;
-    display: flex;
-    color: #ffffff;
-    justify-content: flex-end;
+  .suggestions {
+    top: 40px;
+    right: 0;
+    font-size: 0.9em;
+    opacity: 0.9;
+    border: none;
+    box-shadow: 0 0 30px rgba(0, 0, 0, 0.3);
 
-    .nav {
-      flex: 0 0 auto;
-      display: flex;
-      margin: 0;
-      align-items: center;
+    a {
+      color: #555;
+      font-weight: 400;
+      text-decoration: none;
 
-      .nav-item {
-        margin-left: 20px;
-
-        a {
-          font-size: 18px;
-          color: lighten($accentColor, 40%);
-          text-decoration: none;
-          transition: color 0.3s;
-
-          &:hover {
-            color: lighten($accentColor, 80%);
-          }
-
-          &.nav-link.router-link-exact-active.router-link-active {
-            color: lighten($accentColor, 80%);
-          }
-        }
-      }
-    }
-
-    .search-box {
-      margin-left: 20px;
-
-      input {
-        min-width: 100px;
-        border-radius: 20px;
-        transition: all 0.5s;
+      &.focused {
         color: $accentColor;
-
-        &:hover {
-          border: 1px solid $accentColor;
-          box-shadow: 0 0 5px $accentColor;
-        }
-      }
-
-      .suggestions {
-        top: 40px;
-        right: 0;
-        font-size: 0.9em;
-        opacity: 0.9;
-        border: none;
-        box-shadow: 0 0 30px rgba(0, 0, 0, 0.3);
-
-        a {
-          color: #555;
-          font-weight: 400;
-          text-decoration: none;
-
-          &.focused {
-            color: $accentColor;
-          }
-        }
       }
     }
   }
