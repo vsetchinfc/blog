9faf3de1ec9c9fcf09c6dec4a8986720e7ded3b9
Vlad Setchin <vlad.setchin@gmail.com>
2020-07-16 22:45:50 +1000

    Updated starting song

--- src/.vuepress/theme/layouts/SmulePlayer.vue
+++ src/.vuepress/theme/layouts/SmulePlayer.vue
@@ -45,7 +45,7 @@ export default {
                 // { title: 'M', link: ''},
                 { title: 'M', link: ''}
             ],
-            selectedLink: 'https://www.smule.com/recording/%D0%B0%D0%BB%D0%BB%D0%B0-%D0%BF%D1%83%D0%B3%D0%B0%D1%87%D0%B5%D0%B2%D0%B0-%D0%BD%D0%B5-%D0%BE%D1%82%D1%80%D0%B5%D0%BA%D0%B0%D1%8E%D1%82%D1%81%D1%8F-%D0%BB%D1%8E%D0%B1%D1%8F/1399442024_3634485045/frame/box'
+            selectedLink: 'https://www.smule.com/recording/lionel-richie-acoustic-cover-hello-e_ntyas/2356330899_3491500840/frame/box'
         }
     },
     methods: {
