97dac4520a923c87c8f21c004de3259a869e4a7b
Vlad Setchin <vlad.setchin@gmail.com>
2020-07-26 16:03:06 +1000

    Added song

--- src/.vuepress/theme/layouts/SmulePlayer.vue
+++ src/.vuepress/theme/layouts/SmulePlayer.vue
@@ -56,6 +56,7 @@ export default {
                 { title: 'Стань для меня - Shanti_Shanti\'s version', link: 'https://www.smule.com/recording/%D0%B4%D0%B8%D0%BC%D0%B0-%D0%B1%D0%B8%D0%BB%D0%B0%D0%BD-%D1%81%D1%82%D0%B0%D0%BD%D1%8C-%D0%B4%D0%BB%D1%8F-%D0%BC%D0%B5%D0%BD%D1%8F-shanti_shantis-version/378850526_3657648904/frame/box'},
                 { title: 'Кто-то Простит - Polsip\'s version', link: 'https://www.smule.com/recording/%D0%BC%D1%83%D1%80%D0%B0%D1%82-%D0%BD%D0%B0%D1%81%D1%8B%D1%80%D0%BE%D0%B2-%D0%BA%D1%82%D0%BE-%D1%82%D0%BE-%D0%BF%D1%80%D0%BE%D1%81%D1%82%D0%B8%D1%82-polsips-version/1399442024_3660308678/frame/box'},
                 { title: 'Оттепель', link: 'https://www.smule.com/recording/%D0%BF%D0%B0%D1%83%D0%BB%D0%B8%D0%BD%D0%B0-%D0%B0%D0%BD%D0%B4%D1%80%D0%B5%D0%B5%D0%B2%D0%B0-%D0%BE%D1%82%D1%82%D0%B5%D0%BF%D0%B5%D0%BB%D1%8C/1726826069_3660322941/frame/box'},
+                { title: 'Солнце -1', link: 'https://www.smule.com/recording/%D0%B0%D0%BD%D0%B8-%D0%BB%D0%BE%D1%80%D0%B0%D0%BA-%D1%81%D0%BE%D0%BB%D0%BD%D1%86%D0%B5-1/1726826069_3663381403/frame/box'},
                 //{ title: '', link: ''},
             ],
             selectedLink: 'https://www.smule.com/recording/lara-fabian-a-wonderful-life/2356330899_3577288830/frame/box'
