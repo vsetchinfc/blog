2eac1d0f857d12ac70b49a865fcefcd17e24a2ba
Vladislav Setchin <vlad@setchinfc.com.au>
2021-02-16 10:21:38 +1100

    Added testing support

---
+++ src/.vuepress/theme/layouts/CaseConverter.spec.js
@@ -0,0 +1,34 @@
+// import { shallowMount } from '@vuepress/test-utils'
+// import CaseConverter from 'CaseConverter.vue'
+
+// describe('CaseConverter.vue', () => {
+//   it('Check page title', () => {
+//     const msg = 'new message'
+//     const wrapper = shallowMount(HelloWorld, {
+//       propsData: { msg }
+//     })
+//     expect(wrapper.text()).toMatch(msg)
+//   })
+// })
+
+
+// Import the `mount()` method from Vue Test Utils
+import { mount } from '@vuepress/test-utils'
+
+// The component to test
+const MessageComponent = {
+  template: '<p>{{ msg }}</p>',
+  props: ['msg']
+}
+
+test('displays message', () => {
+  // mount() returns a wrapped Vue component we can interact with
+  const wrapper = mount(MessageComponent, {
+    propsData: {
+      msg: 'Hello world'
+    }
+  })
+
+  // Assert the rendered text of the component
+  expect(wrapper.text()).toContain('Hello world')
+})
