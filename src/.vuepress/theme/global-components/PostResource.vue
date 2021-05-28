ee2f9c94ae89120f5c1ba922a279b6d630bfbdb3
Vladislav Setchin <vlad@setchinfc.com.au>
2021-05-28 13:53:32 +1000

    Added more resources

---
+++ src/.vuepress/theme/global-components/PostResource.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="row mt-lg-4 mt-sm-4">
+    <div class="col-lg-5 col-sm-12">
+      <img :src="image" alt="Resource 5" title="Resource 5" class="w-100"/>
+    </div>
+    <div class="col-lg-7 col-sm-12 mt-lg-2 mt-sm-2">
+      <div class="">
+        <a :href="url">{{title}}</a><br>
+      </div>
+      "{{description}}"
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+	props: {
+    image: {
+      type: String
+    },
+    url: {
+      type: String
+    },
+    title: {
+      type: String
+    },
+    description: {
+      type: String
+    },
+  }
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
